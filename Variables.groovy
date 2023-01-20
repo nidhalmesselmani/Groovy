cca47bced33034221c590b1ec4ec23d7b772b566
nidhal <nidhalmesselmani@yahoo.fr>
2023-01-20 15:02:11 +0100

    Variables

---
+++ Variables.groovy
@@ -0,0 +1,12 @@
+
+String name = "Nidhal"
+
+println 'my name is ' + name
+println 'my name is  ${name}'
+println "my name is  ${name}"
+
+def (int a, int b, int c) = [20,40,50]
+
+println a
+println b
+println c
