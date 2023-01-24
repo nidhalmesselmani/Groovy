699ff7d6b944e598c6f22f017dd489e092b1b028
nidhal <nidhalmesselmani@yahoo.fr>
2023-01-24 07:57:36 +0100

    Data Types

---
+++ DataTypes.groovy
@@ -0,0 +1,45 @@
+// byte
+byte b = 10
+// dynamic data type casting def k = 5
+println b
+def k = (byte) 5
+println k.getClass().getName()
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+println "======================"
+// short 
+short s = 10
+println b
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+println "======================"
+// int 
+int i = 1000
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+println "======================"
+// long
+long l = 100000L
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+println "======================"
+// float
+long f = 100.002F
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+println "======================"
+
+// double 
+double d = 100.002D
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+println "======================"
+// char 
+char c = 'A'
+println c 
+// Boolean 
+Boolean flag = true
+println flag
+// String
+String str = "Groovy"
+println str
