7b0ad30a16ec941abd6ad277fb8fb2f3f71c9f73
nidhal <nidhalmesselmani@yahoo.fr>
2023-02-13 19:46:25 +0100

    Input & Output

---
+++ InputOut.groovy
@@ -0,0 +1,19 @@
+/* print "Enter your name: "
+def name =  System.console().readLine()
+println "Hello  $name"
+
+print "Enter first number "
+def num1 =  System.console().readLine().toInteger()
+print "Enter second number "
+def num2 =  System.console().readLine().toInteger()
+
+println "$num1 + $num2 = " +(num1 + num2)
+ */
+
+// Output
+def myName = "Nidhal"
+/* print
+println */
+printf "My name is %s \n", myName
+printf "%s | %s | %d | %.2f \n", ["Nidhal","Messelmani",10, 20.233]
+printf "%-10s | %20s | %d | %.2f \n", ["Nidhal","Messelmani",10, 20.233]
