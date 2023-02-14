c17423ffc3fe3a9189808c64c429d90365547789
nidhal <nidhalmesselmani@yahoo.fr>
2023-02-14 20:57:59 +0100

    OOP | Inheritance Example

---
+++ Cars.groovy
@@ -0,0 +1,18 @@
+class Cars {
+    String name;
+    String color;
+    int model;
+
+    void startEngine(){
+        println ("Engine started for $name")
+
+    }
+
+    void stopEngine(){
+        println ("Engine stoppped for $name")
+    }
+
+    void accelerate(){
+        println ("Accelerating $name")
+    }
+}
